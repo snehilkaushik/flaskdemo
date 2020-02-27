4d3d8c46a01e0cf5faf319ea2dd9389331e908dc
NamanPatwa <patwanaman@gmail.com>
2020-02-27 18:57:59 +0530

    script changed

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install epel-release 
-yum install python3-pip python3-devel gcc nginx
-pip3 install virtualenv 
-pip3 install gunicorn flask
+yum -y install python3-pip python3-devel gcc nginx
+pip3 -y install virtualenv 
+pip3 -y install gunicorn flask
