7efab54b78e7cf6da494437e5f69bde7a1aec7f8
NamanPatwa <patwanaman@gmail.com>
2020-02-27 19:26:47 +0530

    commit

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum -y install epel-release
 yum -y install python-pip python-devel gcc nginx
-pip -y install virtualenv 
-pip -y install gunicorn flask
+pip install virtualenv 
+pip install gunicorn flask
