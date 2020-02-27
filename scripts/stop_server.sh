5ca4f374e8fd2a8b12dad5dce6b209bb371c1b98
NamanPatwa <patwanaman@gmail.com>
2020-02-27 19:37:34 +0530

    commit

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+cp /home/ec2-user/config/nginx.conf /etc/ngnix/
+cp /home/ec2-user/config/flaskdemo.service /etc/systemd/system/
+
+systemctl start flaskdemo 
+systemctl enable flaskdemo
+usermod -a -G ec2-user nginx 
+chmod 710 /home/ec2-user 
+nginx -t
+systemctl start nginx 
+systemctl enable nginx
