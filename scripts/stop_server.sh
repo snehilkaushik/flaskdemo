2f64a0d237292089865cc7a27252eec8a1eff304
NamanPatwa <46834564+NamanPatwa@users.noreply.github.com>
2020-02-28 20:05:27 +0530

    Update stop_server.sh

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 cp /home/ec2-user/config/nginx.conf /etc/nginx/
-cp /home/ec2-user/config/flaskdemo.service /etc/systemd/system/
 
-service start flaskdemo 
-service enable flaskdemo
-usermod -a -G ec2-user nginx 
-chmod 710 /home/ec2-user 
 nginx -t
 service start nginx 
 service enable nginx
