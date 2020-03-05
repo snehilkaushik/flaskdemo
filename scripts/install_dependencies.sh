94b604c6e5b1c434e8e8138d073d038c3e18c34b
NamanPatwa <46834564+NamanPatwa@users.noreply.github.com>
2020-03-05 12:57:22 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -3,7 +3,7 @@ yum -y install epel-release
 yum -y install python-pip python-devel gcc nginx
 pip install virtualenv 
 pip install gunicorn flask
-cd /home/ec2-user/projects/flaskdemo
-git pull
+#cd /home/ec2-user/projects/flaskdemo
+#git pull
 cp /home/ec2-user/projects/flaskdemo/config/flaskdemo.conf /etc/init/
 cp /home/ec2-user/projects/flaskdemo/config/nginx.conf /etc/nginx/nginx.conf
